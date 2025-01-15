e5a8fcd827e1c2baf01d7f5fe8ce249119b40fdc
Leonardo Risco <leo99153@gmail.com>
2025-01-14 20:18:48 -0500

    el ultimo momento antes del desastre(vue pending, react done)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), vue()],
   vite: {
     optimizeDeps: {
       include: ["@apollo/client", "graphql"],
