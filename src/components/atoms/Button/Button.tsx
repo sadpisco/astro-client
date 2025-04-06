b2c957160bf4e67ba470b66e225973eb3505144e
sadpisco <magos@grupocentrico.com>
2025-04-05 19:45:47 -0500

    Ui Button is almost done, improments

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,19 +1,64 @@
-"use client"
-import { Button as ChakraButton } from "@chakra-ui/react";
-import React from "react";
+import { Button as MuiButton } from '@mui/material';
+import type { ButtonOwnProps } from '@mui/material/Button';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface ButtonProps {
-    children: React.ReactNode;
-    onClick: () => void;
-    loading?: any;
-    disabled?: boolean;
-    colorScheme?: string;
-    variant?: "ghost" | "link" | "solid" | "outline";
-    size?: "xs" | "sm" | "md" | "lg";
-};
+const buttonVariants = cva(
+    '',
+    {
+        variants: {
+            variant: {
+                simple_white:
+                    'bg-slate-200 text-black',
+                special_blue:
+                    '',
+                secondary_blue:
+                    '',
+                special_orange:
+                    '',
+                gray: '',
+                table_blue:
+                    '',
+                gradient_modal:
+                    ''
+            },
+            size: {
+                default: 'h-14 px-5 py-3',
+                sm: 'h-9 px-3',
+                lg: 'h-11 px-8',
+                icon: 'h-10 w-10'
+            }
+        },
+        defaultVariants: {
+            variant: 'simple_white',
+            size: 'default'
+        }
+    }
+);
 
-const Button = (props: ButtonProps): React.ReactElement => {
-    return <ChakraButton {...props}>{props.children}</ChakraButton>
+export type UiButtonProps = ButtonOwnProps &
+    VariantProps<typeof buttonVariants> & {
+        className?: string;
+        onClick?: () => void;
+        loading?: boolean;
+        size?: string;
+    }
+
+const Button: React.FC<UiButtonProps> = ({ className, children, onClick, loading, size, ...props }) => {
+    return (
+        <MuiButton
+            onClick={onClick}
+            loading={loading}
+            className={twMerge(
+                buttonVariants({ variant: props.variant, size: size }),
+                className
+            )}
+            {...props}
+        >
+            {!loading ? children : null}
+        </MuiButton>
+    );
 };
 
 export default Button;
--- src/components/atoms/CounterReact/CounterReact.tsx
+++ src/components/atoms/CounterReact/CounterReact.tsx
@@ -1,48 +1,37 @@
 import Button from '@/components/atoms/Button';
-import { Box, Heading, HStack, VStack } from "@chakra-ui/react";
+import { Buildings } from '@phosphor-icons/react';
 import { useState } from "react";
 
 const CounterReact: React.FC = () => {
   const [count, setCount] = useState<number>(0);
-  const [loading, setLoading] = useState<boolean>(false);
 
   return (
-    <Box className='shadow-md p-6 border-2 border-gray-300 rounded-lg'>
-      <VStack>
-        <Heading size="xl">Contador: {count}</Heading>
-        <HStack>
-          <Button
-            size='sm'
-            variant='ghost'
-            loading={loading}
-            colorScheme='blue'
-            disabled
-            onClick={() => {
-              setCount(count + 1)
-              setLoading(true)
-            }}
-          >
-            Incrementar
-          </Button>
-          <Button
-            size='md'
-            variant='outline'
-            onClick={() => setCount(count - 1)}
-          >
-            Decrementar
-          </Button>
-          <Button
-            size='lg'
-            onClick={() => {
-              setCount(0)
-              setLoading(false)
-            }}
-          >
-            Resetear
-          </Button>
-        </HStack>
-      </VStack>
-    </Box>
+    <div className='shadow-md p-6 border-2 border-gray-300 rounded-lg'>
+      <h1 className='text-xl font-bold mb-4'>Contador: {count}</h1>
+      <div className="flex gap-2">
+        <Button
+          onClick={() => {
+            setCount(count + 1)
+          }}
+          startIcon={<Buildings />}
+        >
+          Incrementar
+        </Button>
+        <Button
+          endIcon={<Buildings />}
+          onClick={() => setCount(count - 1)}
+        >
+          Decrementar
+        </Button>
+        <Button
+          onClick={() => {
+            setCount(0)
+          }}
+        >
+          Resetear
+        </Button>
+      </div>
+    </div>
   );
 };
 
