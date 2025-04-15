d669bc337a2aa72ffd2c0a830c79329c1944fcc4
sadpisco <leo99153@gmail.com>
2025-04-15 16:29:55 -0500

    Unused console.log removed

--- src/graphql/queries/getAllPages.ts
+++ src/graphql/queries/getAllPages.ts
@@ -2,6 +2,7 @@ import { gql } from "graphql-tag";
 export default gql`
   query ExampleQuery {
     pages {
+    documentId
       name
       slug
       phosphorIcon
--- src/middlewares/verify-cms.ts
+++ src/middlewares/verify-cms.ts
@@ -9,6 +9,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
         return next();
     };
     const { pages } = await getPages();
+    console.info('Successfully connected to CMS.');
     if (isEmpty(pages)) {
         return new Response(null, {
             status: 302,
